16f7eba31141824fe95c0625da90b320ac2ecc3b
Alena Trushnikova <alena.yu.t@gmail.com>
2020-12-16 12:25:44 -0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create_table "artists", force: :cascade do |t|
+#     t.string   "name"
+#     t.text     "bio"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+#   create_table "genres", force: :cascade do |t|
+#     t.string   "name"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+#   create_table "songs", force: :cascade do |t|
+#     t.string   "name"
+#     t.integer  "artist_id"
+#     t.integer  "genre_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.index ["artist_id"], name: "index_songs_on_artist_id"
+#     t.index ["genre_id"], name: "index_songs_on_genre_id"
+#   end
+
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+
+Artist.create(name: "Kavinsky", bio: "Vincent Belorgey (born July 31, 1975), known professionally as Kavinsky, is a French musician, producer, DJ and actor.")
+
+Genre.create(name: "Synth-pop")
+
+Song.create(name: "Nightcall", artist_id: 1, genre_id: 1)
+
+puts 'done'
+
+
+
